89d458c63d0428d81f2eb59b27bb4cd6c051714b
hooralhusinan <66676227+hooralhusinan@users.noreply.github.com>
2020-06-13 13:33:10 +0300

    not sure if i did ot right

--- ios-hw-1.playground/Contents.swift
+++ ios-hw-1.playground/Contents.swift
@@ -5,9 +5,9 @@
  
  */
 
-let name = "Ahmad"
+var name = "Ahmad"
 
-name = "Salem"
+name = "Ahmad"
 
 
 var myWeight = 60
@@ -15,11 +15,11 @@ var FriendWeight = 80.5
 var height = 1.5
 
 
-var weightSum = myWeight + FriendWeight
+var weightSum = 60 + 80.5
 
 
 
-var bmi = myWeight * (height * height)
+var bmi = 60 * (1.5 * 1.5)
 
 /*
 - - - -
@@ -38,17 +38,26 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
-var isKuwaiti = false
+var firstName1 = "ahmed"
+var lastName1 = "alahmed"
+var email1 = "ahmad@gmail.com"
+var phoneNumber1 = "556"
+var age1 = 24
+var country1 = "kuwait"
+var area1 = "adailiya"
+var password = "6666"
+var isKuwaiti = true
 
 //Person 2
+var firstName2 = "fahad"
+var lastName2 = "alfahad"
+var email2 = "fahad@gmail.com"
+var phoneNumber2 = "3445"
+var age2 = 25
+var country2 = "kuwait"
+var area2 = "qourtuba"
+var password1 = "66466"
+var isKuwaiti1 = true
 
 
 
@@ -60,6 +69,15 @@ var isKuwaiti = false
 
 
 //Person 3
+var firstName3 = "hoor"
+var lastName3 = "alhusinan"
+var email3 = "hoor@gmail.com"
+var phoneNumber3 = "156"
+var age3 = 24
+var country3 = "uae"
+var area3 = "adailiya"
+var password3 = "66"
+var isKuwaiti3 = false
 
 
 
@@ -79,6 +97,10 @@ var isKuwaiti = false
  * قارن اذا عمر الاشخاص اصغر من ١٨
  * قارن اذا عمر الاشخاص يساوي  ١٧
  * قارن اذا عمر الاشخاص لا يساوي  ١٧
-
+ age1 == age3
+ area3 == area1
+ age1 > 18
+ age2 > 18
+ age3 > 18
  
  */
